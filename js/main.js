472c2c522286ed9dadc47b47b3100a38beefaee7
David <investvirt@gmail.com>
2021-09-15 12:25:26 +0300

    Add all my files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+	$('.photo-slider').slick({
+		prevArrow:
+			'<button type="button" class="slick-prev"><img class="arrow-left" src="img/arrow-left.svg"></button>',
+		nextArrow:
+			'<button type="button" class="slick-next"><img class="arrow-right" src="img/arrow-right.svg"></button>'
+	});
+});
